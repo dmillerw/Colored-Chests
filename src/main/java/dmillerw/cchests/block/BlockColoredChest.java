4fc7c3630f7de03408dda920774bc1b7be93a5c9
dmillerw <dmillerw@gmail.com>
2014-03-03 19:34:13 -0800

    Fixed missing textures

--- src/main/java/dmillerw/cchests/block/BlockColoredChest.java
+++ src/main/java/dmillerw/cchests/block/BlockColoredChest.java
@@ -2,9 +2,11 @@ package dmillerw.cchests.block;
 
 import dmillerw.cchests.block.tile.TileColoredChest;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -20,6 +22,11 @@ public class BlockColoredChest extends BlockChest {
 		for (int i = 0; i < 16; i++) {
 			list.add(new ItemStack(this, 1, i));
 		}
+	}
+
+	@Override
+	public IIcon getIcon(int side, int meta) {
+		return Blocks.planks.getIcon(side, meta);
 	}
 
 	@Override
--- src/main/java/dmillerw/cchests/block/BlockDynamicChest.java
+++ src/main/java/dmillerw/cchests/block/BlockDynamicChest.java
@@ -2,9 +2,11 @@ package dmillerw.cchests.block;
 
 import dmillerw.cchests.block.tile.TileDynamicChest;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -18,6 +20,11 @@ public class BlockDynamicChest extends BlockChest {
 	@Override
 	public void getSubBlocks(Item item, CreativeTabs tab, List list) {
 		list.add(new ItemStack(this, 1, 0));
+	}
+
+	@Override
+	public IIcon getIcon(int side, int meta) {
+		return Blocks.planks.getIcon(side, meta);
 	}
 
 	@Override
